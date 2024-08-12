5a9c9e64209b489b22f2d2976a5078b208da5984
Thomas Gelf <thomas@gelf.net>
2024-08-12 14:44:41 +0200

    PollInterfaceTraffic: no agg table right now

--- src/Scenario/PollInterfaceTraffic.php
+++ src/Scenario/PollInterfaceTraffic.php
@@ -4,7 +4,6 @@ namespace IMEdge\SnmpFeature\Scenario;
 
 use IMEdge\Metrics\MetricDatatype;
 use IMEdge\SnmpFeature\DataStructure\DbColumn;
-use IMEdge\SnmpFeature\DataStructure\DbTable;
 use IMEdge\SnmpFeature\DataStructure\DeviceIdentifier;
 use IMEdge\SnmpFeature\DataStructure\Measurement;
 use IMEdge\SnmpFeature\DataStructure\Metric;
@@ -16,7 +15,7 @@ use Ramsey\Uuid\UuidInterface;
 
 #[SnmpTable([new SnmpTableIndex('ifIndex', new Oid('1.3.6.1.2.1.2.2.1.1'))])]
 #[PollingTask(name: 'interfaceTraffic', defaultInterval: 15)]
-#[DbTable('network_interface_status', [
+#[NotYetNoAggDbTable('network_interface_status', [
     'device_uuid' => 'deviceUuid',
     'if_index'    => 'ifIndex',
 ])]
