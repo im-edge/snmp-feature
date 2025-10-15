e604f4f571d6dafa6486dace58123f5b61aca4f4
Thomas Gelf <thomas@gelf.net>
2025-10-15 16:31:30 +0200

    ScenarioDefinitionLoader: just a simple loader

---
+++ src/Polling/ScenarioDefinitionLoader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace IMEdge\SnmpFeature\Polling;
+
+use IMEdge\Json\JsonString;
+
+class ScenarioDefinitionLoader
+{
+    /**
+     * @return ScenarioDefinition[]
+     */
+    public static function fromJsonString(string $json): array
+    {
+        $scenarios = [];
+
+        foreach (JsonString::decode($json) as $scenario) {
+            $scenarios[$scenario->uuid] = ScenarioDefinition::fromSerialization($scenario);
+        }
+
+        return $scenarios;
+    }
+
+    /**
+     * @return ScenarioDefinition[]
+     */
+    public static function fromJsonFile(string $filename): array
+    {
+        return self::fromJsonString(file_get_contents($filename));
+    }
+}
