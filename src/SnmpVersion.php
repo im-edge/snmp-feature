7cb049632748304f401892c0cd2d36c507ced7fe
Thomas Gelf <thomas@gelf.net>
2025-10-15 16:32:04 +0200

    SnmpVersion: comapt for SnmpPacket

--- src/SnmpVersion.php
+++ src/SnmpVersion.php
@@ -7,4 +7,13 @@ enum SnmpVersion: string
     case v1  = '1';
     case v2c = '2c';
     case v3  = '3';
+
+    public function toNewVersion(): \IMEdge\SnmpPacket\SnmpVersion
+    {
+        return match ($this) {
+            self::v1 => \IMEdge\SnmpPacket\SnmpVersion::v1,
+            self::v2c => \IMEdge\SnmpPacket\SnmpVersion::v2c,
+            self::v3 => \IMEdge\SnmpPacket\SnmpVersion::v3,
+        };
+    }
 }
