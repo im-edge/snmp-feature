c1b582ae78c9f48cadfaf12bc4fb14d02d4b8d0b
Thomas Gelf <thomas@gelf.net>
2026-02-16 10:50:30 +0100

    SnmpTableIndex: allow serialization

--- src/DataStructure/SnmpTableIndex.php
+++ src/DataStructure/SnmpTableIndex.php
@@ -2,7 +2,9 @@
 
 namespace IMEdge\SnmpFeature\DataStructure;
 
-class SnmpTableIndex
+use IMEdge\Json\JsonSerialization;
+
+class SnmpTableIndex implements JsonSerialization
 {
     public function __construct(
         public string $name,
@@ -10,5 +12,18 @@ class SnmpTableIndex
         public bool $implicit = true,
         public readonly ?int $length = 1,
     ) {
+    }
+
+    /**
+     * @return static|SnmpTableIndex
+     */
+    public static function fromSerialization($any): SnmpTableIndex
+    {
+        return new SnmpTableIndex($any[0], new Oid($any[1]), $any[2], $any[3]);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [$this->name, (string) $this->oid, $this->implicit, $this->length];
     }
 }
