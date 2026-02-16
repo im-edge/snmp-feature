645fd814c52163851dc01d9c23efdf7bc5d14fc4
Thomas Gelf <thomas@gelf.net>
2026-02-16 12:57:08 +0100

    SnmpResponse: fix/improve serialization

--- src/SnmpResponse.php
+++ src/SnmpResponse.php
@@ -12,17 +12,28 @@ class SnmpResponse implements JsonSerialization
         public readonly SocketAddress $source,
         public readonly mixed $result = null,
         public readonly ?string $errorMessage = null,
-        public readonly ?float $duration = null,
+        public readonly ?int $duration = null, // Nanoseconds
     ) {
     }
 
     public static function fromSerialization($any): SnmpResponse
     {
-        return new SnmpResponse(...(array) $any);
+        return new SnmpResponse(
+            $any->success,
+            SocketAddress::fromSerialization($any->source),
+            $any->result,
+            $any->errorMessage ?? null,
+            $any->duration ?? null,
+        );
+    }
+
+    public function toArray(): array
+    {
+        return array_filter(get_object_vars($this), fn($v) => $v !== null);
     }
 
     public function jsonSerialize(): object
     {
-        return (object) array_filter(get_object_vars($this), fn($v) => $v !== null);
+        return (object) $this->toArray();
     }
 }
