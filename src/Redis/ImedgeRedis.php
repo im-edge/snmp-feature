533efdb03d518d2ce0f33effc1b0b4684bf347f5
Thomas Gelf <thomas@gelf.net>
2026-02-17 15:34:29 +0100

    ImedgeRedis: add subscriber, tables

--- src/Redis/ImedgeRedis.php
+++ src/Redis/ImedgeRedis.php
@@ -3,7 +3,11 @@
 namespace IMEdge\SnmpFeature\Redis;
 
 use Amp\Redis\RedisClient;
+use Amp\Redis\RedisSubscriber;
+use IMEdge\Inventory\NodeIdentifier;
 use IMEdge\Node\ApplicationContext;
+use IMEdge\RedisTables\RedisTables;
+use Psr\Log\LoggerInterface;
 
 use function Amp\Redis\createRedisClient;
 use function Amp\Redis\createRedisConnector;
@@ -21,7 +25,28 @@ class ImedgeRedis
 
         return createRedisClient(
             $redisUri,
-            new NamedRedisConnector($clientName, createRedisConnector($redisUri))
+            self::connector($clientName, $redisUri)
         );
+    }
+
+    public static function subscriber(string $clientName, ?string $redisUri = null): RedisSubscriber
+    {
+        return new RedisSubscriber(self::connector($clientName, $redisUri));
+    }
+
+    public static function tables(
+        NodeIdentifier $node,
+        LoggerInterface $logger,
+        string $clientName,
+        ?string $redisUri = null
+    ): RedisTables {
+        return new RedisTables($node->uuid->toString(), self::client($clientName, $redisUri), $logger);
+    }
+
+    public static function connector(string $clientName, ?string $redisUri = null): NamedRedisConnector
+    {
+        $redisUri ??= self::getDefaultUri();
+
+        return new NamedRedisConnector($clientName, createRedisConnector($redisUri));
     }
 }
